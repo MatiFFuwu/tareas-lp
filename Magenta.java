e3666f8843e1532b08be5ecfadc27f6246922b8d
Matías <116522687+MatiFFuwu@users.noreply.github.com>
2023-10-06 01:36:10 -0300

    Delete Magenta.java

--- Magenta.java
+++
@@ -1,9 +0,0 @@
-public class Magenta extends Pikinim {
-    public Magenta(){
-        super(2, 1, 10);
-    }
-    public void multiplicar(int cantidad){
-        setCantidad(getCantidad() + cantidad * getAtaque());
-    }
-
-}
