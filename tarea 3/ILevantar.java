302120a21c92ac0cc0eee5af90d9bdda44323fcd
Matías <116522687+MatiFFuwu@users.noreply.github.com>
2023-10-06 01:41:55 -0300

    Create ILevantar.java

---
+++ tarea 3/ILevantar.java
@@ -0,0 +1,12 @@
+public interface ILevantar {
+    public void Levantar(Pikinim cyan, Pikinim magenta, Pikinim amarillo);
+}
+/************
+* Parametros: none
+* **********
+* Retorno: void
+* **********
+* Descripción: Declara el metodo Levantar que debe implementar
+Pieza y Enemigo.
+************
+*/
